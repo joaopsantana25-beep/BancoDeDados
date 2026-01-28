2ec84e2d2c885ad02e4aab280e89c34b89af8c84
João Santana <jpsantana121318@gmail.com>
2026-01-28 16:47:50 -0300

    Alterações no código SQL

--- 28-01-2026/Primeiro SQL.sql
+++ 28-01-2026/Primeiro SQL.sql
@@ -1,7 +1,18 @@
-use escolasenai;usuarios 
-create table usuarios(
+use escolasenai;
+
+/*create table usuarios(
 id int primary key auto_increment,
 nome varchar(100),
 email varchar(100) unique,
 idade int
 );
+*/
+
+/*
+insert into usuarios (nome,email,idade) Values
+("Marcos Silva Antonio", "marcos.aruelio28@orkut",12);
+
+*/
+
+select*from usuarios;
+
